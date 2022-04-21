9441c261da0acf475c2c53ec40cde76dc48112df
ymzEmre <dev.yilmazemre@gmail.com>
2022-04-21 14:13:13 +0300

    Add routing structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,29 @@
 <script>
+	import { Router, Link, Route } from "svelte-navigator";
 	import SurveyJoin from "./components/SurveyJoin.svelte";
+	import { navigate } from "svelte-navigator";
+	import Surveys from "./components/Surveys.svelte";
+
+	function goToSurvey() {
+		navigate("/surveys")
+	}
 </script>
 
 
-<SurveyJoin />
+<Router>
+	<main>
+	  <Route path="/">
+		<SurveyJoin on:surverJoin={goToSurvey}/>
+	  </Route>
+  
+	  <Route path="surveys">
+		<Surveys />
+	  </Route>
+	</main>
+  </Router>
 
 <style lang="postcss" global>
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-  </style>
+</style>
