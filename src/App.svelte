6d7873044afc8a5b45d5badc314ae49bb9c2cfdc
ymzEmre <dev.yilmazemre@gmail.com>
2022-05-05 07:26:47 +0300

    Add component StackOverflow

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
 	import { navigate } from "svelte-navigator";
 	import Surveys from "./components/Surveys.svelte";
 	import Github from './components/Github.svelte';
+	import StackOverflow from './components/StackOverflow.svelte';
 
 	function goToSurvey() {
 		navigate("/surveys")
@@ -23,6 +24,7 @@
 	</main>
   </Router>
 	<Github {cardSettigns}/>
+	<StackOverflow {cardSettigns}/>
 
 <style lang="postcss" global>
 	@tailwind base;
