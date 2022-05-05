5f6a618db1e79f8be827ec406672a88cd8aefbc8
ymzEmre <dev.yilmazemre@gmail.com>
2022-05-05 07:29:59 +0300

    Add component Google Gmail

---
+++ src/components/GoogleGmail.svelte
@@ -0,0 +1,34 @@
+<script>
+  const appUrl = 'https://mail.google.com/mail/u/0/#search/'
+  const appLogo = 'https://docs.google.com/uc?export=open&id=14lHoL1v2sJ7LmMVbUeKRrBN9a0acos8T'
+  let searchContent = null;
+  const placeholder = 'Google Gmail';
+  
+  export let cardSettigns;
+  
+  $: searchUrl = `${appUrl}${searchContent}`;
+  
+  const goToApp = () => {
+    if (searchContent) return window.open(searchUrl);
+  }
+  
+  const onKeyPress = e => {
+    if (e.charCode == 13) goToApp();
+  };
+</script>
+
+  <div class="{cardSettigns.classContainer}">
+    <a href="https://mail.google.com" target="_blank" class="self-center "><img class="self-center w-full h-96 md:h-16 object-contain md:w-16" src="{appLogo}" alt="" /></a>
+    <div class="p-6 flex flex-col justify-start">
+      <div class="flex justify-center">
+          <div class="input-group relative flex flex-wrap items-stretch w-full mb-4">
+            <input type="search" bind:value="{searchContent}" on:keypress={onKeyPress} class="{cardSettigns.classInput}" placeholder="{placeholder}" aria-label="{placeholder}" aria-describedby="button-addon2">
+            <button on:click="{goToApp}" class="btn inline-block px-6 py-2.5 bg-gray-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-500 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
+              <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+              </svg>
+            </button>
+          </div>
+      </div>
+    </div>
+  </div>
